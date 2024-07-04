c0caaa09881286620055d0bd22aedcd96f336cf2
手瓜一十雪 <nanaeonn@outlook.com>
2024-07-04 23:06:40 +0800

    fix: typo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from './common/session';//核心类
 export * from './entities/';//结构类型导出
-export * from './listeners/index';//listener结构导出
-export * from './services/index';//server导出
+export * from './listeners/index';//listeners结构导出
+export * from './services/index';//services导出
